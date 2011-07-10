7412f8ee210ca657b295c557e9bb6a4c87d55e45
unknown <Dax@.(none)>
2011-07-10 12:05:10 -0500

    Removed dead files

--- Sudoku/Types/ICountable.cs
+++
@@ -1,8 +0,0 @@
-//using System.Collections.Generic;
-
-//namespace Sudoku.Types {
-//    public interface ICountable<out T> : IEnumerable<T> {
-//        int Count { get; }
-//        T this[int i] { get; }
-//    }
-//}
